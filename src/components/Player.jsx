20b82b85f7f14224182dade8ecc7856ea380cf77
Prahlad Gaur <65204011+prahladgaur@users.noreply.github.com>
2023-12-30 22:04:42 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -27,9 +27,14 @@ function Player() {
     <>
       <div className="player">
         <video src={videoBg} autoPlay loop muted={muted} />
-        <div className="fixed-btn"><button onClick={mute}>{mutetext}</button></div>
-        <div className="fixed-name">Wishing You A Very Happy New Year {location.state.id}</div> 
-         
+        <div className="fixed-btn">
+          <button onClick={mute}>{mutetext}</button>
+        </div>
+        <div className="fixed-name">
+          Wishing You A Very Happy New Year {location.state.id} || Sorry for all the annoying
+          behavior I did last year. May you give me another chance to do it
+          again in the new year! 
+        </div>
       </div>
     </>
   );
